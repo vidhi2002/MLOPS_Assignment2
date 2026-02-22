2970b5ee0a5bc692e48cf6efecf28b857591716d
Vidhi Mittal <vidhimittal.2002@gmail.com>
2026-02-22 15:59:06 +0530

    Fix CI import issue properly

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlops_project",
-    version="0.1",
-    package_dir={"": "src"},
+    version="0.1.0",
     packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
