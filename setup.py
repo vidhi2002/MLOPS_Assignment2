5b481760a024d601536ec0b65e12cca8ce041207
Vidhi Mittal <vidhimittal.2002@gmail.com>
2026-02-22 15:10:56 +0530

    Fix src layout packaging for CI

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="mlops_project",
     version="0.1",
-    packages=find_packages(),
+    package_dir={"": "src"},    
+    packages=find_packages(where="src"),
 )
